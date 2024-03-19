44311884aee3380f884144c181617f7d508f485e
shu20031026 <shu20031026@gmail.com>
2024-03-19 14:28:04 +0900

    fix

--- src/app/_components/page/root.tsx
+++ src/app/_components/page/root.tsx
@@ -12,7 +12,7 @@ import { handleSignInClick } from "@/lib/firebaseAction";
 import getCookieValue from "@/lib/cookie";
 
 //@ts-ignore
-const token = (typeof window === "object") ? getCookieValue("token") : ""
+const token = (typeof window === "object") ? getCookieValue("token") : "hoge"
 
 // Root
 const Root:FC = () => {
